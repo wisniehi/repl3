9c797e7e07d8a2af5f5bff4ddb39fee9a7bcd1c5
wisniehi <wisniehi@mail.uc.edu>
2021-02-01 19:43:55 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {Scanner s = new Scanner(System.in);
+String name;
+int num;
+
+  System.out.println ("Enter a number to count down or up from: ");
+  name = s.nextLine();
+
+System.out.println("Count to blast off");
+for (int count = 10; count >= 0; count ++)
+{
+  System.out.println(count);
+}
+int c < = 0
+while (c > = 10)
+{
+  System.out.println(c);
+  c= -3
+}
+
+  }
+}
